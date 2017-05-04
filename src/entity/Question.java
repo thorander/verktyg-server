4d02458441ff7a5b29f2596d962e85e49819dd3f
Matilda Thorander <matilda.thorander@gmail.com>
2017-05-04 10:39:23 +0200

    Question class

---
+++ src/entity/Question.java
@@ -0,0 +1,46 @@
+package entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Matilda on 2017-05-04.
+ */
+
+@Entity
+@Table
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String question;
+
+    public Question() {
+        question = "";
+    }
+
+    public Question(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+
+        this.question = question;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+}
